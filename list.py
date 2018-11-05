00c9266eb326723f35d07b4db67d101a03f148a2
Yusril <yusrilchalif@gmail.com>
2018-11-05 09:08:13 +0700

    Update list.py
    fix input & auth

--- list.py
+++ list.py
@@ -1,6 +1,5 @@
-#using pyhton 2.7
-import time
-import tweepy           
+import time 
+import tweepy
 
 #consumer & access token key
 consumer_key        = ''
@@ -8,16 +7,22 @@ consumer_secret_key = ''
 access_token        = ''
 access_token_secret = ''
 
-#OAuth with keys and tokens
+akunVar = raw_input("Account name: ")
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 
-#interface with authentication
-#get followers with user FakhriyRN
 api = tweepy.API(auth)
-ids = []
-for page in tweepy.Cursor(api.followers_ids, screen_name = "FakhriyRN").pages():
-        ids.extend(page)
-        time.sleep(60)
-print ("followers"), len(ids)
 
+users = tweepy.Cursor(api.followers, screen_name = akunVar).items()
+
+#program akan berhenti ketika telah menampilkan setiap 300 akun untuk 15 menit
+while True:
+    try:
+        user = next(users)
+    except tweepy.TweepError:
+        time.sleep(60*15)
+        user = next(users)
+    except StopIteration:
+        break
+    print '@' + user.screen_name
